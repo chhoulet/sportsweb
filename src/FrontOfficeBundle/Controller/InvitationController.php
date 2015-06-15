e40c59d4eee8c32e1445a034fe0233e61ba42c91
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-15 21:40:25 +0200

    Creation de la function response + routing + affichage en homepage du fo

--- src/FrontOfficeBundle/Controller/InvitationController.php
+++ src/FrontOfficeBundle/Controller/InvitationController.php
@@ -28,4 +28,16 @@ class InvitationController extends Controller
 		return $this ->render('FrontOfficeBundle:Invitation:new.html.twig', 
 			array('form'=>$form->createView()));
 	}
+
+	public function responseAction($id)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$invitation = $em -> getRepository('FrontOfficeBundle:Invitation') -> find($id);
+		$invitation -> setAccepted(true);
+		$invitation -> setDateAccepted(new \DateTime('now'));
+
+		$this ->get('session') ->getFlashBag('sucess', 'Vous venez d\'accepter cette invitation. Un e-mail de confirmation vient de vous être envoyé. Bon match !');
+
+		return $this-> redirect($this -> generateUrl('front_office_homepage'));
+	}
 }
