2e25f78e1fa66b26b831a70dd7e714f605ade7c8
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-15 12:55:42 +0200

    Stats du homepage du BO

--- src/BackOfficeBundle/Controller/HomepageController.php
+++ src/BackOfficeBundle/Controller/HomepageController.php
@@ -8,7 +8,10 @@ class HomepageController extends Controller
 {
     public function homepageAction()
     {
-    	
+    	$em = $this -> getDoctrine()->getManager();
+    	$invitations           = $em -> getRepository('FrontOfficeBundle:Invitation') -> nbInvitations();
+    	$nbInvitationsAccepted = $em -> getRepository('FrontOfficeBundle:Invitation') -> nbInvitationsAccepted();
+    	$nbInvitationsDenied   = $em -> getRepository('FrontOfficeBundle:Invitation') -> nbInvitationsDenied();
  
         return $this -> render('BackOfficeBundle:Homepage:homepage.html.twig', 
         	array('invitations'          => $invitations,
