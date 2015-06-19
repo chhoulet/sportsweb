11b193a0b09d2a82c067334ee11d19a6fe49a849
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-19 23:09:47 +0200

    Creation de la function delete User

--- src/BackOfficeBundle/Controller/UserController.php
+++ src/BackOfficeBundle/Controller/UserController.php
@@ -25,4 +25,14 @@ class UserController extends Controller
 
 		return $this -> redirect($this->generateurl('back_office_admin_user'));
 	}
+
+	public function deleteAction($id)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$user = $em -> getRepository('UserBundle:User') -> find ($id);
+		$em -> remove($user);
+		$em -> flush();
+
+		return $this -> redirect($this -> generateUrl('back_office_admin_user'));
+	}
 }
