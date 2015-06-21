279300adc9ee0e57c00d4ad2ac7c32a6fd56e59b
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-21 23:51:27 +0200

    Generation et configuration du formulaire GroupPlayer

---
+++ src/FrontOfficeBundle/Form/GroupPlayerType.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace FrontOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class GroupPlayerType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('practicedSport','choice', array('choices' => array('Basket'   => 'Basket',
+                                                                      'Football' => 'Football')))
+            ->add('typeOfGame')
+            ->add('club')
+            ->add('habitsOfGame')
+            ->add('groupComment')
+            ->add('ground')
+            ->add('Valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeBundle\Entity\GroupPlayer'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficebundle_groupplayer';
+    }
+}
