62a014bfd80518d68c823d52ba8a2355b124a91c
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-21 23:46:35 +0200

    Generation et configuration du formulaire TeamType

---
+++ src/FrontOfficeBundle/Form/TeamType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace FrontOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class TeamType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('sportPracticed','choice', array('choices'=> array('Basket'  => "Basket",
+                                                                     'Football'=> "Football")))
+            ->add('typeOfGame')
+            ->add('habitsOfGame')
+            ->add('teamComment')
+            ->add('ground')
+            ->add('Valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeBundle\Entity\Team'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficebundle_team';
+    }
+}
