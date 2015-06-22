ee08c2a1bdda2948c660f99fe25ced7bf2d68794
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-22 17:21:14 +0200

    Function response pour validationAdmin à la creation d'une team

--- src/BackOfficeBundle/Controller/TeamController.php
+++ src/BackOfficeBundle/Controller/TeamController.php
@@ -16,4 +16,16 @@ class TeamController extends Controller
 		return $this -> render('BackOfficeBundle:Team:list.html.twig', 
 			array('team'=> $team));
 	}
+
+	public function responseAction($id)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$team = $em -> getRepository('FrontOfficeBundle:Team') ->find($id);
+		$team -> setValidationAdmin(true);
+		$team -> setDateValidated(new \datetime('now'));
+		$em -> persist($team);
+		$em -> flush();
+
+		return $this -> redirect($this -> generateUrl('back_office_list_team'));
+	}
 }
