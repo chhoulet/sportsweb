fa5a6d9e90873ff1486703ec7cd268fce10c08ff
Christophe Houlet <chhoulet@yahoo.fr>
2015-06-27 17:56:02 +0200

    Function delete message OK en BO

--- src/BackOfficeBundle/Controller/MessageController.php
+++ src/BackOfficeBundle/Controller/MessageController.php
@@ -13,4 +13,14 @@ class MessageController extends Controller
 
 		return $this ->render('BackOfficeBundle:Message:list.html.twig', array('messages'=>$messages));
 	}
+
+	public function deleteAction($id)
+	{
+		$em = $this -> getDoctrine()->getManager();
+		$message = $em -> getRepository('FrontOfficeBundle:Message')->find($id);
+		$em -> remove($message);
+		$em -> flush();
+
+		return $this -> redirect($this -> generateUrl('back_office_message_list'));
+	}
 }
