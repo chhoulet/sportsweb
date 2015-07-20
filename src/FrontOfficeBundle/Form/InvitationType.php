9874910179b2977e074f3536a2be3e4b4d3a8bc2
Christophe Houlet <chhoulet@yahoo.fr>
2015-07-20 19:03:24 +0200

    Mise en forme du formulaire de lancement d'invitation

--- src/FrontOfficeBundle/Form/InvitationType.php
+++ src/FrontOfficeBundle/Form/InvitationType.php
@@ -15,13 +15,18 @@ class InvitationType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('content')
-            ->add('dateInvit', 'date')
-            ->add('sport', null,                      array('expanded' => true))
-            ->add('mode','choice',  array('choices'=> array( 'Loisir'     => "Loisir",
-                                                             'Compétition'=> "Compétition")))
-            ->add('place')
-            ->add('ground')
+            ->add('content','text',     array('label'  =>'Texte libre:',
+                                              'attr'   => array('placeholder'=>'contexte, joueurs invités ...')))
+            ->add('dateInvit', 'date',  array('label'  =>'Date de la recontre:'))
+            ->add('sport', null,        array('label'  =>'Sport pratiqué:',
+                                              'attr'   => array('expanded'   => true)))
+            ->add('mode','choice',      array('label'  =>'Mode de jeu:',
+                                              'choices'=> array('Loisir'     => "Loisir",
+                                                                'Compétition'=> "Compétition")))
+            ->add('place', 'text',      array('label'  =>'Ville de la rencontre:',
+                                              'attr'   => array('placeholder'=>'Bobigny, Versailles, Tours, Paris 15, ...')))
+            ->add('ground','text',      array('label'  =>'Terrain de la rencontre:',
+                                              'attr'   => array('placeholder'=>'Hall Carpentier, stade Jean Mouchotte, bois de Vincennes,...')))
             ->add('Lancer', 'submit')
         ;
     }
