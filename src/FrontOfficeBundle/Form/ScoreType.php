58a8aba5aead059e9709f914d4548c3694d86440
Christophe Houlet <chhoulet@yahoo.fr>
2015-10-05 13:56:34 +0200

    Retrait du lien d'ajout a sa liste d'amis dans listUsers sur son profil

---
+++ src/FrontOfficeBundle/Form/ScoreType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace FrontOfficeBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ScoreType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('scoreTeam1')
+            ->add('scoreTeam2')                    
+            ->add('Valider','submit')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FrontOfficeBundle\Entity\Matche'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'frontofficebundle_matche';
+    }
+}
