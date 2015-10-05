41d8740e6ef25a2a252f2f75da352983af7565ff
Christophe Houlet <chhoulet@yahoo.fr>
2015-10-05 18:55:29 +0200

    Configuration de messages flash dans la gestion des Teams en BO

--- src/BackOfficeBundle/Controller/TeamController.php
+++ src/BackOfficeBundle/Controller/TeamController.php
@@ -5,6 +5,7 @@ namespace BackOfficeBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FrontOfficeBundle\Entity\Team;
 use FrontOfficeBundle\Form\TeamType;
+use Symfony\Component\HttpFoundation\Request;
 
 class TeamController extends Controller
 {
@@ -29,26 +30,30 @@ class TeamController extends Controller
 	}
 
 	# Validation admin de la team selectionnee:
-	public function responseAction($id)
+	public function responseAction(Request $request, $id)
 	{
 		$em = $this -> getDoctrine()->getManager();
+		$session = $request -> getSession();
 		$team = $em -> getRepository('FrontOfficeBundle:Team') ->find($id);
 		$team -> setValidationAdmin(true);
 		$team -> setDateValidated(new \datetime('now'));
 		$em -> persist($team);
 		$em -> flush();
 
+		$session -> getFlashbag()->add('succes','Cette team est validée !');
 		return $this -> redirect($this -> generateUrl('back_office_list_team'));
 	}
 
 	# Suppression d'une team :
-	public function deleteAction($id)
+	public function deleteAction(Request $request, $id)
 	{
 		$em = $this -> getDoctrine()->getManager();
+		$session = $request -> getSession();
 		$team = $em -> getRepository('FrontOfficeBundle:Team') ->find($id);
 		$em -> remove($team);
 		$em -> flush();
 
+		$session -> getFlashbag()->add('notice','Cette équipe est supprimée !');
 		return $this -> redirect($this -> generateUrl('back_office_list_team'));
 	}
 
